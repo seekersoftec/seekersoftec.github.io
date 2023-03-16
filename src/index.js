a03160fe9fdece7ddc866b2a1f9af6980d627e68
seekersoftec <seekersoftec@gmail.com>
2023-03-16 21:01:27 +0100

    renamed assests to assets

--- src/index.js
+++ src/index.js
@@ -4,22 +4,21 @@ import { BaseProvider, LightTheme } from "baseui";
 import { Provider as StyletronProvider } from "styletron-react";
 import { Client as Styletron } from "styletron-engine-atomic";
 
-
 import "./index.css";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import "./assests/font-awesome/css/all.css";
+import "./assets/font-awesome/css/all.css";
 
 const engine = new Styletron();
 
 ReactDOM.render(
-	<StyletronProvider value={engine}>
-		<BaseProvider theme={LightTheme}>
-				<App />
-	</BaseProvider>
-	</StyletronProvider>,
-	document.getElementById("root")
+  <StyletronProvider value={engine}>
+    <BaseProvider theme={LightTheme}>
+      <App />
+    </BaseProvider>
+  </StyletronProvider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
